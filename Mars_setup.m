a29c568214b1efce93be3ca2b53ae02f97c539f8
Hlpred <71465959+Hlpred@users.noreply.github.com>
2025-02-08 23:07:01 -0500

    Mars Capture Started

--- Mars_setup.m
+++ Mars_setup.m
@@ -32,7 +32,7 @@ v_inf = norm(transfer_data.v_encounter);
 approach_dir = transfer_data.v_encounter / v_inf;
 delta = r*sqrt(1 + 2*Mars_planet.mu/(r*v_inf^2));
 
-offset_dir = cross(approach_dir,[0;1;0])/norm(cross(approach_dir,[0;1;0]));
+offset_dir = cross(approach_dir,[0;sqrt(2)/2;sqrt(2)/2])/norm(cross(approach_dir,[0;sqrt(2)/2;sqrt(2)/2]));
 
 r_orbiter = -1e9*approach_dir + delta*offset_dir;
 v_orbiter = transfer_data.v_encounter;
@@ -41,4 +41,12 @@ orbiter_initial = struct("r_initial", r_orbiter, ...
                           "v_initial", v_orbiter, ...
                           "a_centripetal", 0);
 
-open("Mars_sim.slx");
+delta_v = v_inf*sqrt(2)/2;
+little_delta = 2*asin(1/(1+(r*v_inf^2/Mars_planet.mu)));
+
+plane = cross(v_orbiter, r_orbiter)/norm(cross(v_orbiter, r_orbiter));
+rot = axang2rotm([plane',-deg2rad(45)]);
+
+capture = delta_v*rot*v_orbiter/norm(v_orbiter);
+
+%open("Mars_sim.slx");
